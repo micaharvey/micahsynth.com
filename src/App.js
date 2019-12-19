2e326591c78408b2244238161dd33ed9a3afcf2d
Micah Arvey <micaharvey@gmail.com>
2019-12-18 19:36:28 -0800

    Mac DL Beta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ms from './packages/MicahSynth0.8.0.pkg.zip'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href={ms} download>Micah Synth Beta Download (Mac Only)</a>
       </header>
     </div>
   );
